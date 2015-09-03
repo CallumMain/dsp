c6bfeb651096b5d7a3dba337ee3bd4b801b763b1
CallumMain <CallumMain@gmail.com>
2015-09-02 21:01:27 -0400

    Update advanced_python_csv.py
    Completed function

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,19 @@
-PLACE YOUR CODE HERE
+import re, csv
+def email(data):
+	EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
+	emails = []
+	for line in data:
+		for s in line:
+			emails.extend(re.findall(EMAIL_REGEX, s))
+
+	return emails
+
+with open('faculty.csv', 'rb') as csvfile:
+		reader = csv.reader(csvfile, delimiter=',')
+		parsed_data = list(reader)
+
+emails = email(parsed_data)
+
+writer = csv.writer(open('emails.csv', 'wb'))
+for e in emails:
+	writer.writerow([e])
