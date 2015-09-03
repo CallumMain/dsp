1f3ed260c601eee09fa5e879e0dfb981090d2500
CallumMain <CallumMain@gmail.com>
2015-09-02 21:56:00 -0400

    Update advanced_python_regex.py
    Completed functions

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,41 @@
-PLACE YOUR CODE HERE
+import re, csv
+
+def titles(data):
+	TITLE_REGEX = re.compile(r"\Professor\b")
+	title = []
+	for line in data:
+		for s in line:
+			if TITLE_REGEX.search(s) is not None:
+				title.append(s)
+
+	title.sort()
+	d = {x:title.count(x) for x in title}
+	a,b = d.keys(), d.values()
+
+	print a,b
+
+def email(data):
+	EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
+	emails = []
+	for line in data:
+		for s in line:
+			emails.extend(re.findall(EMAIL_REGEX, s))
+
+	print emails
+
+def domains(data):
+	unique = set()
+	for line in data:
+		for s in line:
+			domain = re.search("@[\w.]+", s)
+			if domain != None:
+				unique.add(domain.group())
+
+	print list(unique)
+
+
+with open('faculty.csv', 'rb') as csvfile:
+		reader = csv.reader(csvfile, delimiter=',')
+		parsed_data = list(reader)
+
+titles(parsed_data)
